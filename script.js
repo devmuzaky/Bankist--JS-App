1004a9aa8b18f3bf5947e075e985f053d1a5b951
Mohamed Zaky <devmuzaky@gmail.com>
2022-10-08 21:12:05 +0200

    implement Loan

--- script.js
+++ script.js
@@ -171,8 +171,18 @@ btnTransfer.addEventListener('click', function (e) {
     }
 })
 
-// Handle close button
+// Handle Loan Account
+btnLoan.addEventListener('click', function (e) {
+    e.preventDefault()
+    const amount = +inputLoanAmount.value
+    if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+        currentAccount.movements.push(amount)
+        updateUi(currentAccount)
+    }
+    inputLoanAmount.value = '';
+})
 
+// Handle close button
 btnClose.addEventListener('click', function (e) {
     e.preventDefault()
     if (
